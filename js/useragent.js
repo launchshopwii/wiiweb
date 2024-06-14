8c6cb4f7547e1a00bcce36f230a215268b59d8db
andrew <118364011+trlandrew@users.noreply.github.com>
2024-06-14 22:12:31 +0500

    Update useragent.js

--- js/useragent.js
+++ js/useragent.js
@@ -1,9 +1,8 @@
 var IsWii = navigator.userAgent.toLowerCase().indexOf("wii") > -1; // returns "true" if internet channel is being used
 var IsNotWii = navigator.userAgent.toLowerCase().indexOf("wii") <= -1; // returns "true" if other browser is being used
 if (IsWii) {
-	void(0); // do nothing
+  void(0); // do nothing
 }
 else if (IsNotWii) {
-	alert("You are not using Internet Channel. Please enter this website on the Wii.");
-	window.location.replace("https://nsc.ftolnks.xyz/"); // redirect to nsc.ftolnks.xyz
+  window.location.replace("https://nsc.ftolnks.xyz/#wiiredirect"); // redirect to nsc.ftolnks.xyz
 }
