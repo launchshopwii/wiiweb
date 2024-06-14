2985fe121574e43d1ac615f5f9fef0c6e6ffccab
LNLenost <niccolo.salerno.2010@gmail.com>
2024-06-14 19:46:36 +0200

    New translations gallery.php (Polish)

--- translations/pl/gallery.php
+++ translations/pl/gallery.php
@@ -1,32 +1,32 @@
-    <?php
+<?php
 $title = "Gallery";
 include($_SERVER['DOCUMENT_ROOT'].'/elements/head.php'); ?>
 
 <body class="body-dark">
     <div class="container pt-5">
-        <div style="margin-left: 300px;"><h1>Gallery</h1></div>
+        <a href="btn btn-white" onclick="history.back()">Back</a>
+        <h1>Gallery</h1>
         <div class="gallery-container">
-            <img src="../../img/Logo.png" class="gallery-img" alt="New Shop Channel's Logo">
-            <img src="../../img/wallpaper1.png" class="gallery-img" alt="Wallpaper 1">
-            <img src="../../img/wallpaper2.png" class="gallery-img" alt="Wallpaper 2">
-            <img src="../../img/wallpaper3.png" class="gallery-img" alt="Wallpaper 3">
+            <img src="img/Logo.png" class="gallery-img" alt="New Shop Channel's Logo">
+            <img src="img/wallpaper1.png" class="gallery-img" alt="Wallpaper 1">
+            <img src="img/wallpaper2.png" class="gallery-img" alt="Wallpaper 2">
+            <img src="img/wallpaper3.png" class="gallery-img" alt="Wallpaper 3">
         </div>
         <p style="margin-left: 75px" class="gallery-text">Logo</p>
         <p style="margin-left: 100px;" class="gallery-text">Wallpaper 1</p>
         <p style="margin-left: 75px" class="gallery-text">Wallpaper 2</p>
         <p style="margin-left: 80px;" class="gallery-text">Wallpaper 3</p>
         <div>
-            <img src="../../img/wallpaper4.png" class="gallery-img" alt="Wallpaper 4">
-            <img src="../../img/wallpaper5.png" class="gallery-img" alt="Wallpaper 5">
-            <img src="../../img/wallpaper6.png" class="gallery-img" alt="Wallpaper 6">
-            <img src="../../img/wallpaper7.png" class="gallery-img" alt="Wallpaper 7">
+            <img src="img/wallpaper4.png" class="gallery-img" alt="Wallpaper 4">
+            <img src="img/wallpaper5.png" class="gallery-img" alt="Wallpaper 5">
+            <img src="img/wallpaper6.png" class="gallery-img" alt="Wallpaper 6">
+            <img src="img/wallpaper7.png" class="gallery-img" alt="Wallpaper 7">
         </div>
         <p style="margin-left: 35px" class="gallery-text">Wallpaper 4</p>
         <p style="margin-left: 85px;" class="gallery-text">Wallpaper 5</p>
         <p style="margin-left: 80px" class="gallery-text">Wallpaper 6</p>
         <p style="margin-left: 75px;" class="gallery-text">Wallpaper 7</p>
         <br>
-        <div style="margin-left: 300px;"><a class="btn btn-primary" href="../../index.php">Back</a></div>
     </div>
 </body>
 </html>
