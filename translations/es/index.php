c17cf03913dc04f4770284cef25fc1bf299c4613
LNLenost <niccolo.salerno.2010@gmail.com>
2024-06-14 10:19:34 +0200

    New translations index.php (Spanish)

---
+++ translations/es/index.php
@@ -0,0 +1,25 @@
+<?php
+$title = "Casa";
+include($_SERVER['DOCUMENT_ROOT'].'/elements/head.php');
+?>
+
+<body class="body-dark">
+    <div class="container pt-5">
+        <h1>
+            <img src="img/Logo_Wii_sm.png" width="56" class="header-img" rel="preload" alt="Logo">
+            <span class="header-text">New Shop Channel</span>
+        </h1>
+        <div class="content">
+            <p>
+                <strong>Use the B button to scroll down!</strong><br>
+                New Shop Channel is a project driven by the community with the aim of bringing back the same experience of the Wii Shop Channel for the Wii. Remember when you could download channels, WiiWare, and Virtual Console titles? Or maybe you never experienced that? Well, with New Shop Channel you can experience it again!
+                New Shop Channel can run on any Wii console, it's region-free and also works on Wii U via Wii Mode. New Shop Channel provides WiiWare, Virtual Consoles, and Wii Channels.
+            </p>
+            <p>
+                This community-driven project relies on the support and contributions of dedicated fans and developers. Its availability could change, so please stay connected to the community channels for any updates, assistance, and announcements. Check us on one of our social media platforms below.
+            </p>
+            <a href="gallery.html" class="btn btn-primary">Wallpapers</a>&nbsp;<a href="discord.html" style="margin-top: 10px" class="btn btn-primary">Discord</a>&nbsp;<a href="source.html" style="margin-top: 10px" class="btn btn-secondary">GitHub</a>&nbsp;<a href="blog.html" style="margin-top: 10px" class="btn btn-primary">Blog</a>&nbsp;<a href="privacypolicy.html" class="btn btn-secondary">Privacy Policy</a>
+        </div>
+    </div>
+</body>
+</html>
