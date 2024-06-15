82c61645ec60fa5711017c4f99f5dfda37833833
LNLenost <niccolo.salerno.2010@gmail.com>
2024-06-15 14:16:28 +0200

    New translations blog.php (German)

--- translations/de/blog.php
+++ translations/de/blog.php
@@ -5,7 +5,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/elements/head.php'); ?>
 <body class="body-dark">
     <div class="container">
         <h1>
-        <img src="img/Logo_Wii_sm.png" width="56" class="header-img" rel="preload" alt="Logo">
+        <img src="../../img/Logo_Wii_sm.png" width="56" class="header-img" rel="preload" alt="Logo">
         <span class="header-text">Latest Blog Post</span>
         </h1>
           <p>
@@ -21,6 +21,7 @@ Ever wanted to check the website on your Wii? No? Well, now you can! Sort of. It
 I thought it would be a good idea to remake the translation system, but it resulted in many errors, so I decided to schedule it for later. This update could add faster text loading and make it easier adding new languages. Another upcoming feature is translation statistics, which will show how much of the website is translated in a specific language (e.g. Turkish at 66.67% and Italian at 100%). However, there was an issue where the statistics incorrectly found untranslated strings, like if the Italian and English translations matched, which I’ll need to fix later.
 That is all. We are the New Shop Channel.
       </p>
+      <div align='center'><a href="../../index.php" class="btn btn-primary">Home Page</a></div>
     </div>
 </body>
 </html>
